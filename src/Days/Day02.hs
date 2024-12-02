a3858bac9c359882386476af9b2e6e5ab5046567
Thomas Stacey <Thomas@ThomasStacey.co.uk>
2024-12-02 10:10:20 +0000

    Day 2

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -8,18 +8,32 @@ runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
 
 testDay :: String -> String -> Spec
-testDay = T.testDay parser part1 part2 0 0
+testDay = T.testDay parser part1 part2 2 4
 
-type Input = [Int]
+type Input = [[Int]]
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser = map (map read . words) . lines
 
 part1 :: Input -> Output1
-part1 = undefined
+part1 = length . filter checkSafe
+
+checkSafe :: [Int] -> Bool
+checkSafe xs = checkCond checkDiff xs && (checkCond (<) xs || checkCond (>) xs)
+    where
+        checkCond :: (Int -> Int -> Bool) -> [Int] -> Bool
+        checkCond p xs = and $ zipWith p xs $ tail xs
+
+        checkDiff :: Int -> Int -> Bool
+        checkDiff x y = diff >= 1 && diff <= 3
+            where diff = abs $ x - y
+
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 = length . filter checkSafe'
+
+checkSafe' :: [Int] -> Bool
+checkSafe' xs = any (checkSafe . uncurry (++) . fmap tail . (`splitAt` xs)) [0..length xs - 1]
