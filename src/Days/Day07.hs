07ba03ed0451e8a012881b35d7b6c66321ef791a
Thomas Stacey <Thomas@ThomasStacey.co.uk>
2024-12-07 15:11:40 +0000

    Day 7

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -1,25 +1,50 @@
 module Days.Day07 where
+import           Data.Bifunctor  (bimap)
+import           Data.List.Split (splitOn)
+import           Data.Maybe      (catMaybes)
 import qualified Program.RunDay  as R (runDay)
 import qualified Program.TestDay as T (testDay)
 import           System.Clock    (TimeSpec)
 import           Test.Hspec      (Spec)
+import           Util.Util       (listToTuple)
 
 runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
 
 testDay :: String -> String -> Spec
-testDay = T.testDay parser part1 part2 0 0
+testDay = T.testDay parser part1 part2 3749 11387
 
-type Input = [Int]
+type Input = [(Int, [Int])]
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser = map (bimap read (map read . words) . listToTuple . splitOn ": ") . lines
 
 part1 :: Input -> Output1
-part1 = undefined
+part1 = solve False
+
+checkEquation :: Bool -> (Int, [Int]) -> Bool
+checkEquation _ (_, []) = False
+checkEquation allowConcat (target, first:rest) = go first rest
+    where
+        go :: Int -> [Int] -> Bool
+        go acc [] = target == acc
+        go acc (x:xs)
+            | null attempts = False
+            | otherwise = or attempts
+            where
+                addResult = acc + x
+                mulResult = acc * x
+                concatResult = read @Int (show acc ++ show x)
+                tryAdd = if addResult <= target then Just $ go addResult xs else Nothing
+                tryMul = if mulResult <= target then Just $ go mulResult xs else Nothing
+                tryConcat = if allowConcat && concatResult <= target then Just $ go concatResult xs else Nothing
+                attempts = catMaybes [tryAdd, tryMul, tryConcat]
+
+solve :: Bool -> [(Int, [Int])] -> Int
+solve allowConcat = sum . map fst . filter (checkEquation allowConcat)
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 = solve True
