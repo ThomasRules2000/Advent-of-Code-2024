de1fed88e05638d81ec6933a4217650790626bd8
Thomas Stacey <Thomas@ThomasStacey.co.uk>
2024-12-10 20:08:05 +0000

    Day 10

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -1,25 +1,47 @@
 module Days.Day10 where
+import           Data.Char       (digitToInt)
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import qualified Program.RunDay  as R (runDay)
 import qualified Program.TestDay as T (testDay)
 import           System.Clock    (TimeSpec)
 import           Test.Hspec      (Spec)
+import qualified Util.Map        as Map
 
 runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
 
 testDay :: String -> String -> Spec
-testDay = T.testDay parser part1 part2 0 0
+testDay = T.testDay parser part1 part2 36 81
 
-type Input = [Int]
+type Input = Map (Int, Int) Int
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser = Map.fromGrid . map (map digitToInt) . lines
 
 part1 :: Input -> Output1
-part1 = undefined
+part1 = solve Map.size
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 = solve sum
+
+getAround :: (Int, Int) -> [(Int, Int)]
+getAround (x, y) = [(x-1, y), (x, y-1), (x, y+1), (x+1, y)]
+
+solve :: (Map (Int, Int) Int -> Int) -> Map (Int, Int) Int -> Int
+solve f m = sum $ map (f . getTrails m) $ Map.keys $ Map.filter (== 0) m
+
+getTrails :: Map (Int, Int) Int -> (Int, Int) -> Map (Int, Int) Int
+getTrails m = go 0 . flip Map.singleton 1
+    where
+        go :: Int -> Map (Int, Int) Int -> Map (Int, Int) Int
+        go 9 xs = xs
+        go n xs = go (n+1)
+                $ Map.fromListWith (+)
+                $ concatMap (\(pos, score) -> map (,score)
+                                            $ filter (\x -> Map.lookup x m == Just (n+1))
+                                            $ getAround pos)
+                $ Map.toList xs
