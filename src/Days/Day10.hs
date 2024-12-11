8e10258991d48ffc84ccbba9f0417574d679c9b6
Thomas Stacey <Thomas@ThomasStacey.co.uk>
2024-12-11 23:57:49 +0000

    Make Day 10 slightly nicer

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -2,6 +2,8 @@ module Days.Day10 where
 import           Data.Char       (digitToInt)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
 import qualified Program.RunDay  as R (runDay)
 import qualified Program.TestDay as T (testDay)
 import           System.Clock    (TimeSpec)
@@ -28,20 +30,19 @@ part1 = solve Map.size
 part2 :: Input -> Output2
 part2 = solve sum
 
-getAround :: (Int, Int) -> [(Int, Int)]
-getAround (x, y) = [(x-1, y), (x, y-1), (x, y+1), (x+1, y)]
+getAround :: (Int, Int) -> Set (Int, Int)
+getAround (x, y) = Set.fromAscList [(x-1, y), (x, y-1), (x, y+1), (x+1, y)]
 
 solve :: (Map (Int, Int) Int -> Int) -> Map (Int, Int) Int -> Int
 solve f m = sum $ map (f . getTrails m) $ Map.keys $ Map.filter (== 0) m
 
 getTrails :: Map (Int, Int) Int -> (Int, Int) -> Map (Int, Int) Int
-getTrails m = go 0 . flip Map.singleton 1
+getTrails m = go 0 . (`Map.singleton` 1)
     where
         go :: Int -> Map (Int, Int) Int -> Map (Int, Int) Int
-        go 9 xs = xs
-        go n xs = go (n+1)
-                $ Map.fromListWith (+)
-                $ concatMap (\(pos, score) -> map (,score)
-                                            $ filter (\x -> Map.lookup x m == Just (n+1))
-                                            $ getAround pos)
-                $ Map.toList xs
+        go 9 = id
+        go n = go (n+1)
+             . Map.unionsWith (+)
+             . Map.mapWithKey (\pos score -> Map.fromSet (const score)
+                                           $ Set.filter ((Just (n+1) ==) . flip Map.lookup m)
+                                           $ getAround pos)
